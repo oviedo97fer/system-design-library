43ce18d39d2d2a6f44bcf4b54464f48d818255de
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2024-06-12 18:08:02 -0300

    update DateTimePicker & Input

--- src/Input/index.tsx
+++ src/Input/index.tsx
@@ -1,83 +1,96 @@
 import React from "react";
 import { styled, Theme } from "@mui/system";
 import { alpha } from "@mui/material/styles";
-import {
-  FormControl,
-  InputLabel,
-  InputBase,
-  InputBaseProps,
-  InputAdornment,
-  FormHelperText,
-} from "@mui/material";
+import { FormControl, InputLabel, InputAdornment, FormHelperText } from "@mui/material";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import MuiTextField, { TextFieldProps } from "@mui/material/TextField";
 
 interface Props {
-  label?: string;
-  isPassword?: boolean;
-  helperText?: string;
-  error?: boolean;
-  sx?: any;
+    label?: string;
+    isPassword?: boolean;
+    helperText?: string;
+    error?: boolean;
+    sx?: any;
+    InputProps?: any;
+    isDate?: boolean;
 }
 interface MyTheme extends Theme {
-  transitions: {
-    create: (props: string | string[]) => string;
-  };
+    transitions: {
+        create: (props: string | string[]) => string;
+    };
 }
-const CustomInput =styled(InputBase, { skipSx: false })<Props>(({ theme }: { theme: Theme } & Props) => ({
-  "label + &": {
-      marginTop: theme.spacing(3)
-  },
-  "& .MuiInputBase-input": {
-      borderRadius: 4,
-      position: "relative",
-      backgroundColor: theme.palette.mode === "light" ? "#F3F6F9" : "#1A2027",
-      border: "1px solid",
-      borderColor: theme.palette.mode === "light" ? "#c0c2c5" : "#2D3843",
-      fontSize: 16,
-      padding: "10px 12px",
-      transition: (theme as MyTheme).transitions.create(["border-color", "background-color", "box-shadow"]),
-      "&:focus": {
-          boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-          borderColor: theme.palette.primary.main
-      }
-  }
+export const CustomInput = styled(MuiTextField, { skipSx: false })<Props>(({ theme }: { theme: Theme } & Props) => ({
+    "label + &": {
+        marginTop: theme.spacing(3)
+    },
+    "& .MuiOutlinedInput-root": {
+        transition: (theme as MyTheme).transitions.create(["border-color", "background-color", "box-shadow"]),
+        backgroundColor: theme.palette.mode === "light" ? "#ffffff" : "#1A2027",
+        fontSize: 14,
+        "& fieldset": {
+            borderColor: theme.palette.mode === "light" ? "#c0c2c5" : "#2D3843"
+        },
+        "&:hover fieldset": {
+            borderColor: "#B2BAC2"
+        },
+        "&.Mui-focused fieldset": {
+            boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+            borderColor: theme.palette.primary.main
+        }
+    },
+    "& .MuiInputBase-input": {
+        borderRadius: 4,
+        position: "relative",
+
+        padding: "10px 10px 7px"
+    },
+    "& .Mui-disabled": {
+        backgroundColor: "#eaeaf1"
+    }
 }));
 
-const Input = (props: Props & Omit<InputBaseProps, keyof Props>) => {
-  const { label, id, type, isPassword, fullWidth, helperText, error, sx, ...other } = props;
-  const [inputType, setInputType] = React.useState(
-    isPassword ? "password" : props.type
-  );
+export type TextfieldInputProps = Props & Omit<TextFieldProps, keyof Props>;
 
-  const IconEndAdornment = React.memo(() => (
-    <InputAdornment
-      position="end"
-      data-testid="input-adornment"
-      onClick={() =>
-        setInputType(inputType === "password" ? "text" : "password")
-      }
-    >
-      {inputType === "password" ? <VisibilityOffIcon /> : <VisibilityIcon />}
-    </InputAdornment>
-  ));
+const Input = (props: TextfieldInputProps) => {
+    const { label, id, type, isPassword, fullWidth, helperText, error, sx, ...other } = props;
 
-  return (
-    <FormControl variant="standard" fullWidth={fullWidth} error={error} sx={sx}>
-      <InputLabel shrink htmlFor={id}>
-        {label}
-      </InputLabel>
-      <CustomInput
-        id={id}
-        type={inputType}
-        {...other}
-        {...(isPassword && {
-          endAdornment: <IconEndAdornment />,
-        })}
-      />
-      <FormHelperText>{helperText}</FormHelperText>
-    </FormControl>
-  );
+    console.log({ other });
+    const [inputType, setInputType] = React.useState(isPassword ? "password" : props.type);
+
+    const IconEndAdornment = React.memo(() => (
+        <InputAdornment
+            position="end"
+            data-testid="input-adornment"
+            onClick={() => setInputType(inputType === "password" ? "text" : "password")}
+            sx={{
+                position: "absolute",
+                right: 15
+            }}
+        >
+            {inputType === "password" ? <VisibilityOffIcon /> : <VisibilityIcon />}
+        </InputAdornment>
+    ));
+
+    return (
+        <FormControl variant="standard" fullWidth={fullWidth} error={error} sx={sx}>
+            <InputLabel shrink htmlFor={id}>
+                {label}
+            </InputLabel>
+            <CustomInput
+                id={id}
+                type={inputType}
+                InputProps={{
+                    ...(isPassword && {
+                        endAdornment: <IconEndAdornment />
+                    }),
+                    ...other.InputProps
+                }}
+                {...other}
+            />
+            <FormHelperText>{helperText}</FormHelperText>
+        </FormControl>
+    );
 };
 
 export default Input;
--- src/stories/Components/DateTimePicker.stories.tsx
+++ src/stories/Components/DateTimePicker.stories.tsx
@@ -4,24 +4,22 @@ import { Meta, StoryFn } from "@storybook/react";
 import { DateTimePicker } from "../../index";
 
 export default {
-	title: "Components/DateTime",
-	component: DateTimePicker,
+    title: "Components/DateTime",
+    component: DateTimePicker
 } as Meta;
 
 export const Default: StoryFn = ({ ...args }) => {
-	return (
-		<DateTimePicker
-			label=""
-			name=""
-			value=""
-			language="en"
-			disabled={false}
-			variant="standard"
-			size="small"
-			required
-			readOnly
-			format="L LTS"
-			{...args}
-		/>
-	);
+    return (
+        <DateTimePicker
+            label=""
+            name=""
+            value=""
+            language="en"
+            disabled={false}
+            size="small"
+            required
+            format="L LTS"
+            {...args}
+        />
+    );
 };
