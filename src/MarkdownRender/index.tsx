9af6248820a3d0fd32914ca7417a81b917b55fc7
Fernando Oviedo <37948156+oviedo97fer@users.noreply.github.com>
2024-07-26 17:58:23 -0300

    update markdown

--- src/MarkdownRender/index.tsx
+++ src/MarkdownRender/index.tsx
@@ -1,110 +1,109 @@
 import React from "react";
-import { Button, Checkbox, FormControlLabel, Typography } from "@mui/material";
-import ReactMarkdown, { Components } from "react-markdown";
+
+import MarkdownPreview from "@uiw/react-markdown-preview";
 
 interface MarkdownRenderProps {
-	text: string;
-	onCloseModal?: () => void;
-	onConfirmModal?: () => void;
-	isCentered?: boolean;
-	handleRadioChange?: () => void;
-	checkboxChecked?: boolean;
+    text: string;
+    onCloseModal?: () => void;
+    onConfirmModal?: () => void;
+    isCentered?: boolean;
+    handleRadioChange?: () => void;
+    checkboxChecked?: boolean;
 }
 
 const MarkdownRender: React.FC<MarkdownRenderProps> = ({
-	text,
-	onCloseModal = () => {},
-	onConfirmModal = () => {},
-	isCentered = false,
-	handleRadioChange = () => {},
-	checkboxChecked = false,
+    text
+    /* onCloseModal = () => {},
+    onConfirmModal = () => {},
+    isCentered = false,
+    handleRadioChange = () => {},
+    checkboxChecked = false */
 }) => {
-	const components: Components = {
-		img: ({ node, ...props }) => <img {...props} alt={props.alt} />,
-		a: ({ node, ...props }) => {
-			const href = props.href ?? "";
-			const isButton = href === "#";
-			const isHintLink = href.includes("hint-");
-			const isCloseButton = href.includes("close");
-			// const hintKey = isHintLink ? href.slice(5) : "";
-			const isSimpleLink = href.includes("link:");
+    /*  const components: Components = {
+        img: ({ node, ...props }) => <img {...props} alt={props.alt} />,
+        a: ({ node, ...props }) => {
+            const href = props.href ?? "";
+            const isButton = href === "#";
+            const isHintLink = href.includes("hint-");
+            const isCloseButton = href.includes("close");
+            // const hintKey = isHintLink ? href.slice(5) : "";
+            const isSimpleLink = href.includes("link:");
 
-			if (href === "checkbox") {
-				return (
-					<FormControlLabel
-						control={<Checkbox checked={checkboxChecked} onChange={handleRadioChange} />}
-						label={props.children}
-					/>
-				);
-			} else if (isHintLink) {
-				return (
-					<Button
-						// onClick={() => openModal(hintKey)}
-						sx={{ color: "primary.main" }}
-					>
-						{props.children}
-					</Button>
-				);
-			} else if (isCloseButton) {
-				return (
-					<Button variant="contained" onClick={onCloseModal} sx={{ mt: -3 }}>
-						{props.children}
-					</Button>
-				);
-			} else if (isSimpleLink) {
-				return (
-					<Typography
-						href={href.slice(5)}
-						sx={{ color: "primary.main" }}
-						target="_blank"
-						rel="noopener noreferrer"
-						component="a"
-					>
-						{props.children}
-					</Typography>
-				);
-			} else {
-				return isButton ? (
-					<Button variant="contained" sx={{ mt: 5 }} onClick={isButton ? onConfirmModal : () => {}}>
-						props.children
-					</Button>
-				) : (
-					<Typography
-						href={href}
-						target="_blank"
-						rel="noopener noreferrer"
-						component="a"
-						sx={{ color: "primary.main" }}
-					>
-						{props.children}
-					</Typography>
-				);
-			}
-		},
-		// blockquote: ({ node, ...props }) => (
-		// 	<Grid container alignItems="center" style={{ paddingTop: 24 }}>
-		// 		<Grid item>
-		// 			<img style={{ borderRadius: 0 }} src="/images/icons/note-icon.svg" alt="Note Icon" />
-		// 		</Grid>
-		// 		<Grid item>
-		// 			<Typography variant="subtitle1" style={{ paddingLeft: 8 }}>
-		// 				{props.children}
-		// 			</Typography>
-		// 		</Grid>
-		// 	</Grid>
-		// ),
-		h3: ({ node, ...props }) => <Typography variant="h3">{props.children}</Typography>,
-		h4: ({ node, ...props }) => <Typography variant="h4">{props.children}</Typography>,
-	};
-
-	return (
-		<ReactMarkdown
-			className={`markdown-container ${isCentered ? "markdown-container-centered" : ""}`}
-			components={components}
-		>
-			{text}
-		</ReactMarkdown>
-	);
+            if (href === "checkbox") {
+                return (
+                    <FormControlLabel
+                        control={<Checkbox checked={checkboxChecked} onChange={handleRadioChange} />}
+                        label={props.children}
+                    />
+                );
+            } else if (isHintLink) {
+                return (
+                    <Button
+                        // onClick={() => openModal(hintKey)}
+                        sx={{ color: "primary.main" }}
+                    >
+                        {props.children}
+                    </Button>
+                );
+            } else if (isCloseButton) {
+                return (
+                    <Button variant="contained" onClick={onCloseModal} sx={{ mt: -3 }}>
+                        {props.children}
+                    </Button>
+                );
+            } else if (isSimpleLink) {
+                return (
+                    <Typography
+                        href={href.slice(5)}
+                        sx={{ color: "primary.main" }}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        component="a"
+                    >
+                        {props.children}
+                    </Typography>
+                );
+            } else {
+                return isButton ? (
+                    <Button variant="contained" sx={{ mt: 5 }} onClick={isButton ? onConfirmModal : () => {}}>
+                        props.children
+                    </Button>
+                ) : (
+                    <Typography
+                        href={href}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        component="a"
+                        sx={{ color: "primary.main" }}
+                    >
+                        {props.children}
+                    </Typography>
+                );
+            }
+        },
+        // blockquote: ({ node, ...props }) => (
+        // 	<Grid container alignItems="center" style={{ paddingTop: 24 }}>
+        // 		<Grid item>
+        // 			<img style={{ borderRadius: 0 }} src="/images/icons/note-icon.svg" alt="Note Icon" />
+        // 		</Grid>
+        // 		<Grid item>
+        // 			<Typography variant="subtitle1" style={{ paddingLeft: 8 }}>
+        // 				{props.children}
+        // 			</Typography>
+        // 		</Grid>
+        // 	</Grid>
+        // ),
+        h3: ({ node, ...props }) => <Typography variant="h3">{props.children}</Typography>,
+        h4: ({ node, ...props }) => <Typography variant="h4">{props.children}</Typography>
+    };
+ */
+    return (
+        <MarkdownPreview
+            source={text}
+            style={{ padding: 16 }}
+            /* components={components} */
+        />
+    );
 };
 
 export default MarkdownRender;
--- src/stories/Components/MarkdownRender.stories.tsx
+++ src/stories/Components/MarkdownRender.stories.tsx
@@ -4,15 +4,12 @@ import { Meta, StoryFn } from "@storybook/react";
 import { MarkdownRender } from "../../index";
 
 export default {
-	title: "Components/MarkdownRender",
-	component: MarkdownRender,
+    title: "Components/MarkdownRender",
+    component: MarkdownRender
 } as Meta;
 
 export const Default: StoryFn = ({ ...args }) => {
-	return (
-		<MarkdownRender
-			text="By tapping 'Create account' or 'Login' you agree to our [Conditions of Use.](link:#) Learn how we process your data in our [Privacy Policy and Cookies Policy.](link:#)"
-			{...args}
-		/>
-	);
+    return (
+        <MarkdownRender text="By tapping 'Create account' or 'Login' you agree to our [Conditions of Use.](link:#) Learn how we process your data in our [Privacy Policy and Cookies Policy.](link:#)" />
+    );
 };
