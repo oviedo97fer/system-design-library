3bf41d99277b327a96ce9c60b57ebf6fb5a3a6ad
Fernando Oviedo <37948156+oviedo97fer@users.noreply.github.com>
2024-08-19 13:09:50 -0300

    update Select component

--- src/stories/Components/Select.stories.tsx
+++ src/stories/Components/Select.stories.tsx
@@ -4,10 +4,10 @@ import { Meta, StoryFn } from "@storybook/react";
 import { Select } from "../../index";
 
 export default {
-  title: "Components/Select",
-  component: Select,
+    title: "Components/Select",
+    component: Select
 } as Meta;
 
 export const Default: StoryFn = ({ ...args }) => {
-  return <Select variant="standard" {...args} options={[]} fullWidth />;
+    return <Select variant="outlined" label="Select" {...args} options={[]} fullWidth />;
 };
